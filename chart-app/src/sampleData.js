a3cb83fcd0778d49e9d8b11e5c257bf568400668
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-31 11:27:05 -0700

    Fix style errors

--- chart-app/src/sampleData.js
+++ chart-app/src/sampleData.js
@@ -13,7 +13,7 @@ export const myChart = {
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 159, 64, 0.2)',
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -21,18 +21,18 @@ export const myChart = {
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 159, 64, 1)',
             ],
-            borderWidth: 1
-        }]
+            borderWidth: 1,
+        }],
     },
     options: {
         scales: {
             y: {
-                beginAtZero: true
-            }
-        }
-    }
-};
+                beginAtZero: true,
+            },
+        },
+    },
+}
 
-export default myChart;
+export default myChart
