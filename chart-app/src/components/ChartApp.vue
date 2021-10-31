09d8f9f3826041def4bb30f66d6d768e73c0772a
Ryan Meneses <meneses.ryan.j@gmail.com>
2021-10-31 12:32:25 -0700

    Make PR and eslint style corrections

--- chart-app/src/components/ChartApp.vue
+++ chart-app/src/components/ChartApp.vue
@@ -1,49 +1,59 @@
 <template>
-  <div class="chart">
-    <h1>{{ msg }}</h1>
+    <div class="chart">
+        <h1>{{msg}}</h1>
 
-<!--    This div is the container for the chart component-->
-    <div class="chart-container">
-      <!-- Using Chart.js in a Vue.js Application-->
-      <!-- Step 2. Modify App.vue to use the new Chart component-->
-      <SampleChart/>
+        <!--    This div is the container for the chart component-->
+        <div class="chart-container">
+            <!-- Using Chart.js in a Vue.js Application-->
+            <!-- Step 2. Modify App.vue to use the new Chart component-->
+            <SampleChart />
+        </div>
+
+        <!--    This div is the container for the buttons-->
+        <div class="controls-container">
+
+            <div class="dropdown-container">
+                <select id="chart-select" name="charts">
+                    <option value="" hidden disabled selected>--Please select a chart--</option>
+                    <option v-for="(chart, name) in charts" :key="name" :value="name">{{chart.label}}</option>
+                </select>
+            </div>
+
+            <div class="enter-button-container">
+                <button id="enter-button" name="enter">Enter</button>
+            </div>
+
+            <div class="refresh-button-container">
+                <button id="refresh-button" name="refresh">Refresh</button>
+            </div>
+        </div>
+
     </div>
-
-<!--    This div is the container for the buttons-->
-    <div class="controls-container">
-
-      <div class="dropdown-container">
-        <select name="charts" id="chart-select">
-          <option value="">-- Please select a chart --</option>
-          <option value="chart1">Chart 1</option>
-          <option value="chart2">Chart 2</option>
-          <option value="chart3">Chart 3</option>
-        </select>
-      </div>
-
-      <div class="enter-button-container">
-        <button name="enter" id="enter-button">Enter</button>
-      </div>
-
-      <div class="refresh-button-container">
-        <button name="refresh" id="refresh-button">Refresh</button>
-      </div>
-    </div>
-
-  </div>
 </template>
 
 <script>
 // Using Chart.js in a Vue.js Application
 // Step 2. Modify ChartApp.vue to use the new Chart Component in this Component
-import SampleChart from "@/components/SampleChart";
+import SampleChart from '@/components/SampleChart'
 
 export default {
-  name: 'ChartApp',
-  props: {
-    msg: String
-  },
-  components: {SampleChart}
+    name: 'ChartApp',
+    components: {SampleChart},
+    props: {
+        msg: {
+            type: String,
+            default: '',
+        },
+    },
+    data() {
+        return {
+            charts: {
+                chart1: {label: 'Chart 1'},
+                chart2: {label: 'Chart 2'},
+                chart3: {label: 'Chart 3'},
+            },
+        }
+    },
 }
 </script>
 
@@ -65,13 +75,6 @@ a {
   margin: 0 auto;
 }
 
-/*!* The following CSS to the end of the file are placeholder elements to help with organizing the layout *!*/
-/*.chart-container > .chart-placeholder {*/
-/*  height: 250px;*/
-/*  width: 500px;*/
-/*  background-color: #555;*/
-/*}*/
-
 .controls-container {
   display: flex;
   justify-content: center;
@@ -80,13 +83,7 @@ a {
   margin: 40px 0 0;
 }
 
-/*.controls-container > .dropdown-container {*/
-/*  height: 50px;*/
-/*  width: 280px;*/
-/*  background-color: #555;*/
-/*}*/
-
-.controls-container > .enter-button-container > #enter-button {
+#enter-button {
   height: 50px;
   width: 100px;
   border-radius: 8px;
@@ -95,7 +92,7 @@ a {
   background-color: #42b983;
 }
 
-.controls-container > .refresh-button-container > #refresh-button {
+#refresh-button {
   height: 50px;
   width: 100px;
   border-radius: 8px;
@@ -104,10 +101,9 @@ a {
   background-color: #555;
 }
 
-
-.controls-container > .dropdown-container > #chart-select {
+#chart-select {
   height: 50px;
-  width: fit-content;
+  width: 200px;
   border-radius: 8px;
   border: solid #42b983;
   box-sizing: border-box;
