ab9755e33440c238033f43f8083810a18233701c
Ian Castellanos <imcastellanos@yahoo.com>
2021-11-02 20:15:09 -0700

    Task#207 H2 for wiki description using v-if

--- chart-app/src/router/index.js
+++ chart-app/src/router/index.js
@@ -6,33 +6,33 @@ import Wiki from '../views/Wiki'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
-    }
-  },
-  {
-    path: '/wiki',
-    name: 'Wiki',
-    component: Wiki
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component() {
+            return import(/* webpackChunkName: "about" */ '../views/About')
+        },
+    },
+    {
+        path: '/wiki',
+        name: 'Wiki',
+        component: Wiki,
+    },
 ]
 
 const router = new VueRouter({
-  routes: [
-    // using redirect route to show syntax
-    { path: '/wikipedia', redirect: '/wiki'}
-  ]
+    route: [
+        {path: '/wikipedia', redirect: '/wiki'},
+    ],
+    routes,
 })
 
 export default router
--- chart-app/tests/unit/example.spec.js
+++ chart-app/tests/unit/example.spec.js
@@ -1,12 +1,12 @@
-import { shallowMount } from '@vue/test-utils'
+import {shallowMount} from '@vue/test-utils'
 import ChartApp from '@/components/ChartApp.vue'
 
 describe('ChartApp.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(ChartApp, {
-      propsData: { msg }
+    it('renders props.msg when passed', () => {
+        const msg = 'new message'
+        const wrapper = shallowMount(ChartApp, {
+            propsData: {msg},
+        })
+        expect(wrapper.text()).toMatch(msg)
     })
-    expect(wrapper.text()).toMatch(msg)
-  })
 })
