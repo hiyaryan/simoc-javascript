e4443ced549e998a31eef1fb7202bd4905b9dc7d
Ian Castellanos <imcastellanos@yahoo.com>
2021-11-02 20:30:07 -0700

    Task#206 Add H1 for title on wiki page

--- chart-app/src/views/Wiki.vue
+++ chart-app/src/views/Wiki.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="wiki">
         <h1>Wiki Placeholder</h1>
+        <h1>Title for Wiki Page</h1>
 
-        <!-- This is a place holder for which chart will be passed from the parent component-->
+        <!-- This is a place holder for which chart will be passed from the parent component
+        this will need to be modified for component state once that task is completed -->
         <h2 v-if="chart1">Chart1</h2>
         <h2 v-if="chart2">Chart2</h2>
         <h2 v-if="chart3">Chart3</h2>
